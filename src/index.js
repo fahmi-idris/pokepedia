9b5a1a4a8180b4cda650f78292cff540c65ad1cf
Frontend Developer <fidriz5@gmail.com>
2018-09-26 02:23:03 +0700

    Installing reactstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Application/App';
 import "./Application/Assets/style.scss"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './Infrastructure/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
