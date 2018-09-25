7af9e5a223140504955cf1b5eccef86a0edcc427
Frontend Developer <fidriz5@gmail.com>
2018-09-26 02:19:54 +0700

    Install sass loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Application/App';
+import "./Application/Assets/style.scss"
 import registerServiceWorker from './Infrastructure/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
